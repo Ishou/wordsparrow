1ec354fb3ec871937fada5f4741417f3e098eb2a
Ishou <Ishou@users.noreply.github.com>
2026-06-14 14:54:57 +0200

    chore(deps): update plugin org.jetbrains.kotlin.plugin.serialization to v2.4.0
    Signed-off-by: Renovate Bot <bot@wordsparrow.io>

--- identity/api/build.gradle.kts
+++ identity/api/build.gradle.kts
@@ -7,7 +7,7 @@
 
 plugins {
     kotlin("jvm")
-    kotlin("plugin.serialization") version "2.3.21"
+    kotlin("plugin.serialization") version "2.4.0"
     application
     id("com.gradleup.shadow") version "9.4.1"
 }
